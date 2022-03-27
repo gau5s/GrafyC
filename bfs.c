afeb68cf7e17186af488d70fddb26f4906495eaf
Krzysztof Jastrzebski <toja@example.com>
2022-03-27 22:57:36 +0200

    bfs

--- bfs.c
+++ bfs.c
@@ -3,39 +3,49 @@
 #include "pq.h"
 #include "bfs.h"
 #include "init.h"
+#define SIZE 16
+
 
 int bfs(graph_t* gr,int height,int width)
-{ 
-	int i,n=1;
-	int* visited = malloc(sizeof(int)*height*width);
-	pq_t pq = init_pq( height*width );
-	visited[0]=1;
-	add_to_pq(pq,0);
-	while(not_empty_pq(pq))
+{
+	pq_t pq = init_pq(SIZE);
+	d_t* visited = malloc(sizeof(d_t) * height*width);
+	visited[0].odl = 0;
+	visited[0].k = 0;
+	visited[0].p = 0;
+	add_to_pq(pq, visited[0]);
+
+	for(int k = 1; k < height*width; k++)
+	{
+		visited[k].p = -1;
+	}
+
+	d_t i;
+	while(not_empty_pq( pq ))
 	{
 		i = pop_from_pq(pq);
-		for(int j=0;j<4;j++)
+		for(int j = 0; j < 4; j++)
 		{
-			if(gr[i].edg[j]!=NULL && visited[gr[i].edg[j]->node]!=1)
-			{	
-                	printf("%d\n",gr[i].edg[j]->node);
-			add_to_pq(pq,gr[i].edg[j]->node);	
-			visited[gr[i].edg[j]->node]=1;
-			n++;
+			if(gr[i.k].edg[j] != NULL && visited[gr[i.k].edg[j]->node].p == -1) 
+			{
+				int x = gr[i.k].edg[j]->node;
+				visited[x].k = x;
+				visited[x].p = i.k;
+				visited[x].odl = gr[i.k].val_edg[j];
+				add_to_pq(pq, visited[x]);
 			}
 		}
 	}
-	free_pq( pq );
-        free(visited);
-	if(n==height*width)
-        {
-                printf("\nGraf spojny\n");
-                return 1;
-        }
-        else
-        {
-                printf("\nGraf nie spojny\n");
-                return -1;
-        }
 
+
+	for(int k = 0; k < height* width; k++) 
+	{
+		if(visited[k].p == -1) 
+		{
+			printf("\nGraf niespojny\n");
+			return 0;
+		}
+	}
+	printf("\nGraf spojny\n");
+	return 1;
 }
